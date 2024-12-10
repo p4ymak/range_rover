835f2d4a09f9d0dc9cb927eda6a682b932444e4a
Roman Chumak <p4ymak@yandex.ru>
2024-12-10 20:44:58 +0300

    RangeTree

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,104 @@
 use num::{Bounded, Integer, One, Saturating};
-use std::{collections::BTreeMap, ops::RangeInclusive};
+use std::{mem, ops::RangeInclusive};
 
-/// Takes a custom sequence of non-repeating integers and produces a sorted vector of ranges.
+#[derive(Debug)]
+pub struct RangeTree<T> {
+    range: RangeInclusive<T>,
+    less: Option<Box<RangeTree<T>>>,
+    more: Option<Box<RangeTree<T>>>,
+}
+
+impl<A: Copy + Clone + Integer + Saturating + One + Bounded> FromIterator<A> for RangeTree<A> {
+    fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
+        let mut input = iter.into_iter();
+        if let Some(first) = input.next() {
+            let mut tree = RangeTree::new(first);
+            for value in input {
+                tree.insert(value);
+            }
+            tree
+        } else {
+            RangeTree::<A>::new(A::zero())
+        }
+    }
+}
+
+impl<T: Copy + Clone + Integer + Saturating + One + Bounded> RangeTree<T> {
+    pub fn new(value: T) -> Self {
+        RangeTree {
+            range: value..=value,
+            less: None,
+            more: None,
+        }
+    }
+
+    pub fn insert(&mut self, value: T) {
+        if self.range.contains(&value) {
+            return;
+        }
+        if self.range.start() > &value {
+            if self.less.is_none() {
+                self.less = Some(Box::new(RangeTree::new(value)));
+            }
+            if let Some(less) = &mut self.less {
+                less.insert(value);
+                if less.range.end().saturating_add(T::one()) == *self.range.start() {
+                    self.range = *less.range.start()..=*self.range.end();
+                    self.less = mem::take(&mut less.less);
+                }
+            }
+        } else if self.range.end() < &value {
+            if self.more.is_none() {
+                self.more = Some(Box::new(RangeTree::new(value)));
+            }
+            if let Some(more) = &mut self.more {
+                more.insert(value);
+                if self.range.end().saturating_add(T::one()) == *more.range.start() {
+                    self.range = *self.range.start()..=*more.range.end();
+                    self.more = mem::take(&mut more.more);
+                }
+            }
+        }
+    }
+
+    pub fn to_vec(&self) -> Vec<RangeInclusive<T>> {
+        let mut vec = Vec::<RangeInclusive<T>>::new();
+        self.to_vec_req(&mut vec);
+        vec
+    }
+
+    fn to_vec_req(&self, vec: &mut Vec<RangeInclusive<T>>) {
+        if let Some(less) = &self.less {
+            less.to_vec_req(vec);
+        }
+        if let Some(last) = vec.last_mut() {
+            if last.end().saturating_add(T::one()) == *self.range.start() {
+                *last = *last.start()..=*self.range.end();
+            } else {
+                vec.push(self.range.clone());
+            }
+        } else {
+            vec.push(self.range.clone());
+        }
+
+        if let Some(more) = &self.more {
+            more.to_vec_req(vec);
+        }
+    }
+}
+
+/// Takes a custom sequence of integers and produces a sorted vector of ranges.
 /// # Examples
 /// ```
 /// use range_rover::range_rover;
-/// let input = vec![2, 0, 7, 10, 1, 3, 6, 4, 5, 9, 8];
+/// let input = vec![2, 0, 7, 10, 4, 1, 3, 6, 4, 0, 5, 9, 8];
 /// let result = range_rover(input);
 /// assert_eq!(result, vec![0..=10]);
 /// ```
 ///
 /// ```
 /// use range_rover::range_rover;
-/// let input = vec![-1, -2, 2, 0, 7, 10, -4, 1, 3, 6, -3, 4, 9, 8];
+/// let input = vec![-1, -2, 2, 0, 7, 10, -4, 1, 3, 6, -3, 10, 4, 9, 8, -2];
 /// let result = range_rover(input);
 /// assert_eq!(result, vec![-4..=4, 6..=10]);
 /// ```
@@ -21,37 +107,14 @@ where
     I: IntoIterator<Item = T>,
     T: Copy + Clone + Integer + Saturating + One + Bounded,
 {
-    let mut map = BTreeMap::<T, T>::new(); // key: end, value: start
-    for i in input {
-        let e = i.saturating_add(T::one());
-        if let Some(s) = map.remove(&i) {
-            map.insert(e, s);
-        } else {
-            map.insert(e, i);
+    let mut ranges = vec![];
+    let mut input = input.into_iter();
+    if let Some(first) = input.next() {
+        let mut tree = RangeTree::new(first);
+        for value in input {
+            tree.insert(value);
         }
-    }
-    let mut ranges = Vec::<RangeInclusive<T>>::new();
-    let mut map_iter = map.into_iter();
-    let mut add_range = |from, to| {
-        ranges.push(
-            from..=(to
-                - match to == T::max_value() {
-                    true => T::zero(),
-                    false => T::one(),
-                }),
-        )
-    };
-    if let Some((mut to, mut from)) = map_iter.next() {
-        for r in map_iter {
-            if to == r.1 {
-                to = r.0;
-            } else {
-                add_range(from, to);
-                from = r.1;
-                to = r.0;
-            }
-        }
-        add_range(from, to);
+        ranges = tree.to_vec();
     }
     ranges
 }
@@ -62,15 +125,15 @@ mod tests {
     use rand::Rng;
     #[test]
     fn max_value() {
-        let input = (u32::MAX - 10)..=u32::MAX;
+        let input = (u16::MIN..=u16::MAX).rev();
         let result = range_rover(input);
-        assert_eq!(result, vec![(u32::MAX - 10)..=u32::MAX]);
+        assert_eq!(result, vec![(u16::MIN)..=u16::MAX]);
     }
 
     #[test]
     fn random() {
         let mut rng = rand::thread_rng();
-        let mut random_numbers = [0_usize; 1024];
+        let mut random_numbers = [0_usize; 1024 * 4];
         rng.fill(&mut random_numbers);
 
         let mut input_sorted = random_numbers.to_vec();
